8a51d38d4290b3a13b2a1cc46fe1079b870eb554
Daryoush1989 <daryoushwaheed@hotmail.com>
2026-05-01 16:22:31 +0100

    Align security module variables for staging database

--- infra/modules/security/variables.tf
+++ infra/modules/security/variables.tf
@@ -1,15 +1,33 @@
 variable "name_prefix" {
-  description = "Name prefix for security resources."
+  description = "Name prefix used for security resources."
   type        = string
+}
+
+variable "environment" {
+  description = "Environment name, for example staging or prod."
+  type        = string
+  default     = "staging"
 }
 
 variable "vpc_id" {
-  description = "VPC ID."
+  description = "VPC ID where security groups are created."
   type        = string
 }
 
+variable "app_port" {
+  description = "Application container port."
+  type        = number
+  default     = 8080
+}
+
+variable "db_port" {
+  description = "PostgreSQL database port."
+  type        = number
+  default     = 5432
+}
+
 variable "tags" {
-  description = "Common tags."
+  description = "Common tags for resources."
   type        = map(string)
   default     = {}
 }
